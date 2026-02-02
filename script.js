a6aaa06f015764b07e6ca874961767502400c409
slashline15 <slashline15@gmail.com>
2026-02-01 23:58:00 -0400

    adiciona arquivos iniciais: launch.json, index.html e script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+var nomeDaVariavel = "Valor da variável";
+var outraVariavel = "Valor da outra variável";
+var variavelIdade = 32;
+var tenhoCabeloBranco = false;
+var variavelIndefinida = undefined;
+var variavelNula = null;
+ 
+console.log( nomeDaVariavel );
+console.log( outraVariavel );
+console.log( variavelIdade );
+console.log( tenhoCabeloBranco );
+console.log( variavelIndefinida );
+console.log( variavelNula );
